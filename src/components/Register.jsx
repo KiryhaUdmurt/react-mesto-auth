e0d011b3c3a0c5a603e78d48b94d60b1b84463ac
Kirill <kalinin1507.2012@gmail.com>
2023-05-09 22:24:07 +0400

    fix in progress

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import useForm from "../hooks/useForm";
+import useForm from "../hooks/useFormAndValidation";
 import useFormAndValidation from "../hooks/useFormAndValidation";
 
 const Register = ({ registerUser, onInfoClick }) => {
